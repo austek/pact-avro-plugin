460909d3172537f7ce38082c0d8ee19d2f1d6278
austek <ali.ustek@collibra.com>
2023-02-16 16:32:40 +0000

    Add Avro Schema to configuration

---
+++ modules/core/src/main/scala/com/collibra/plugin/avro/AvroSchemaBase16Hash.scala
@@ -0,0 +1,17 @@
+package com.collibra.plugin.avro
+
+import com.google.common.io.BaseEncoding
+import org.apache.avro.Schema
+
+import java.security.MessageDigest
+
+case class AvroSchemaBase16Hash(value: String)
+
+object AvroSchemaBase16Hash {
+  def apply(avroSchema: Schema): AvroSchemaBase16Hash = {
+    val digest: MessageDigest = MessageDigest.getInstance("MD5")
+    digest.update(avroSchema.toString.getBytes)
+    val avroSchemaHash: String = BaseEncoding.base16().lowerCase().encode(digest.digest())
+    AvroSchemaBase16Hash(avroSchemaHash)
+  }
+}
